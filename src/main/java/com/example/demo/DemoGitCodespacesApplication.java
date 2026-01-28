0121db5cf3107d6841499ffe9e54fd18388fa6c2
skc4365 <50658047+skc4365@users.noreply.github.com>
2026-01-28 11:14:03 +0900

    Initial commit

---
+++ src/main/java/com/example/demo/DemoGitCodespacesApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGitCodespacesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoGitCodespacesApplication.class, args);
+	}
+
+}
